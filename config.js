128fffe53401bab0c6e3fdc4d0605b85ff96e556
Eric Frick <eric.frick@icloud.com>
2013-10-23 21:25:23 -0400

    Changed config.js to standard Heroku variables

--- config.js
+++ config.js
@@ -9,8 +9,8 @@ config.database.user     = process.env.DB_USER     || "root";
 config.database.password = process.env.DB_PASS     || "password";
 config.database.socket   = "/opt/mysql/mysql.sock";
 
-config.app.environment   = process.env.APP_ENV     || "development";
-config.app.port          = process.env.NODE_PORT   || 8080;
+config.app.environment   = process.env.NODE_ENV    || "development";
+config.app.port          = process.env.PORT        || 8080;
 config.app.address       = process.env.NODE_IPADDR || "localhost";
 
 module.exports = config;
